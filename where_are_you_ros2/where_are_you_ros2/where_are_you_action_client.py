6969999e6216a2be1ff59da7df95e2d21aa01f53
Luca Beber <luca.beber@gmail.com>
2024-11-20 09:28:17 +0100

    Add Rotate action definition and update dependencies; modify submodule reference and presentation files

--- where_are_you_ros2/where_are_you_ros2/where_are_you_action_client.py
+++ where_are_you_ros2/where_are_you_ros2/where_are_you_action_client.py
@@ -4,80 +4,129 @@ import rclpy
 from rclpy.action import ActionClient
 from rclpy.node import Node
 from nav_msgs.msg import Odometry  # For reading /odom data
-from uwb_interfaces.msg import UwbRange
+from sensor_msgs.msg import Range
 from action_move_interfaces.action import MoveDistance  # Import custom action
+from action_move_interfaces.action import Rotate
 import numpy as np
 from scipy.optimize import minimize
 import matplotlib.pyplot as plt
+import tf_transformations
+import os
 
-def find_P(dist, D_fixed):
-    n = len(dist)+1
-    D = D_fixed
-    # Complete D_fixied with the first row and column
-    for i in range(1,n):
-        D[0][i] = dist[i-1]**2
-        D[i][0] = dist[i-1]**2
-    
-    # Compute the Gram matrix
-    H = np.eye(n) - (1/n) * np.ones((n, n))
-    G = -0.5 * H @ D @ H
-    
-    # Compute the eigenvalues and eigenvectors of the Gram matrix
-    V, U = np.linalg.eig(G) 
-    
-    # Extract the eigenvectors corresponding to the eigenvalues different from 0
-    U = U[:, V > 1e-6]
-    V = np.diag(V[V > 1e-6])
+class WhereAreYou():
+    def __init__(self):
+        
+        self.anchor_dist = 2
+        # Initialize the matrix of distances between the anchors
+        self.D_fixed = np.array([[0, 0, 0],
+                                [0, 0, self.anchor_dist],
+                                [0, self.anchor_dist, 0]])
+        
 
-    # Compute P
-    P = (U @ np.sqrt(V)).T
+        # Initialize the estimated points P_k, P_k1, and P_k2
+        self.P_k = np.zeros((2, 3))
+        self.P_k1 = np.zeros((2, 3))
+        self.P_k2 = np.zeros((2, 3))
 
-    # Translate the points of P so that the first point is at the origin
-    P = P - P[:, 0].reshape(-1, 1)
+        self.P_relative = np.zeros((2, 3))
 
-    return P
+    def where_are_you(self, dist_1, dist_2, dist_3):
+        # Compute the P matrices for the new matrices
+        P_k = self.find_P(dist_1, self.D_fixed)
+        P_k1 = self.find_P(dist_2, self.D_fixed)
+        P_k2 = self.find_P(dist_3, self.D_fixed)
 
-def find_roto_translation(P_pre, P_cur, t_k):
-    theta = 0
-    T = np.array([0,0])
+        print("P_k:\n", P_k)
+        print("P_k1:\n", P_k1)
+        print("P_k2:\n", P_k2)
 
-    alpha = 1
-    
-    def fun(x):
-        R = np.array([[np.cos(x[0]), -np.sin(x[0])],
-                  [np.sin(x[0]),  np.cos(x[0])]])
-        return np.linalg.norm(P_pre[:, 1:3] - (R @ (alpha * S @ P_cur[:, 1:3]) + np.array([[x[1]],[x[2]]])), ord='fro')
-    
-    # def fun_reflected(x):
-    #     R = np.array([[np.cos(x[0]), -np.sin(x[0])],
-    #               [np.sin(x[0]),  np.cos(x[0])]])
-    #     return np.linalg.norm(P_pre[:, 1:3] - (R @ (alpha * S @ P_cur[:, 1:3]) + np.array([[x[1]],[x[2]]])), ord='fro') 
+        # Compute the relative rotation and translation between the matrices
+        R_k, T_k = find_roto_translation(P_k, P_k1, t_k)
+        # Find the rotation matrix between vector T and vector t_k
+        u = T_k / np.linalg.norm(T_k)
+        v = np.array([1, 0])
+        theta = np.arctan2(v[1], v[0]) - np.arctan2(u[1], u[0])
 
-    x0 = np.array([theta, T[0], T[1]])
-    # Decrease minimum step size to increase precision
-    options = {'disp': True, 'maxiter': 100}  # Options for the optimizer
+        # Construct the rotation matrix
+        R_in = np.array([[np.cos(theta[0]), -np.sin(theta[0])],
+                        [np.sin(theta[0]),  np.cos(theta[0])]])   
+        P_k = R_in @ P_k
 
-    S = np.eye(2)
-    # First optimization using 
-    result = minimize(fun, x0, method='BFGS', options=options)
-    x = result.x
-    R = np.array([[np.cos(x[0]), -np.sin(x[0])],
-                [np.sin(x[0]),  np.cos(x[0])]]) @ S
-    T = np.array([[x[1]],[x[2]]])
-    print("Translation Norm:", np.linalg.norm(T))
+        # Rototranslate the P_k1 in the new frame
+        P_k1_new = R_in @ (R_k @ P_k1 + T_k)
 
-    if abs(np.linalg.norm(T-P_pre[:,0]) - np.linalg.norm(t_k)) > np.linalg.norm(t_k)*0.05:
-        # Second optimization with reflected S
-        S = np.diag([-1, 1])
-        result_reflected = minimize(fun, x0, method='BFGS', options=options)
-        x = result_reflected.x
+        # # Compute the relative rotation and translation between the matrices
+        R_k1, T_k1 = find_roto_translation(P_k1_new, P_k2, t_k1+t_k)
+
+        # Rototranslate the P_k2 in the new frame
+        P_k2_new = R_k1 @ P_k2 + T_k1
+
+
+    # Find the P matrix that represents the points in the new frame
+    def find_P(dist, D_fixed):
+        n = len(dist)+1
+        D = np.copy(D_fixed)
+        # Complete D_fixied with the first row and column
+        for i in range(1,n):
+            D[0][i] = dist[i-1]**2
+            D[i][0] = dist[i-1]**2
+        
+        # Compute the Gram matrix
+        H = np.eye(n) - (1/n) * np.ones((n, n))
+        G = -0.5 * H @ D @ H
+        
+        # Compute the eigenvalues and eigenvectors of the Gram matrix
+        V, U = np.linalg.eig(G) 
+        
+        # Extract the eigenvectors corresponding to the eigenvalues different from 0
+        U = U[:, V > 1e-6]
+        V = np.diag(V[V > 1e-6])
+
+        # Compute P
+        P = (U @ np.sqrt(V)).T
+
+        # Translate the points of P so that the first point is at the origin
+        P = P - P[:, 0].reshape(-1, 1)
+
+        return P
+
+    # Find the relative rotation and translation between two matrices
+    def find_roto_translation(P_pre, P_cur, t_k):
+        theta = 0
+        T = np.array([0,0])
+
+        alpha = 1
+        
+        def fun(x):
+            R = np.array([[np.cos(x[0]), -np.sin(x[0])],
+                    [np.sin(x[0]),  np.cos(x[0])]])
+            return np.linalg.norm(P_pre[:, 1:3] - (R @ (alpha * S @ P_cur[:, 1:3]) + np.array([[x[1]],[x[2]]])), ord='fro')
+
+        x0 = np.array([theta, T[0], T[1]])
+        # Decrease minimum step size to increase precision
+        options = {'disp': True, 'maxiter': 100}  # Options for the optimizer
+
+        S = np.eye(2)
+        # First optimization using 
+        result = minimize(fun, x0, method='BFGS', options=options)
+        x = result.x
         R = np.array([[np.cos(x[0]), -np.sin(x[0])],
                     [np.sin(x[0]),  np.cos(x[0])]]) @ S
         T = np.array([[x[1]],[x[2]]])
         print("Translation Norm:", np.linalg.norm(T))
 
-    
-    return R, T
+        if abs(np.linalg.norm(T-P_pre[:,0]) - np.linalg.norm(t_k)) > np.linalg.norm(t_k)*0.05:
+            # Second optimization with reflected S
+            S = np.diag([-1, 1])
+            result_reflected = minimize(fun, x0, method='BFGS', options=options)
+            x = result_reflected.x
+            R = np.array([[np.cos(x[0]), -np.sin(x[0])],
+                        [np.sin(x[0]),  np.cos(x[0])]]) @ S
+            T = np.array([[x[1]],[x[2]]])
+            print("Translation Norm:", np.linalg.norm(T))
+
+        
+        return R, T
 
 class MoveDistanceClient(Node):
 
@@ -86,6 +135,7 @@ class MoveDistanceClient(Node):
 
         # Action client for the move_distance action
         self._action_client = ActionClient(self, MoveDistance, 'move_distance')
+        self._action_client_rotate = ActionClient(self, Rotate, 'rotate')
 
         # Subscribe to /odom for tracking movement
         self.subscription = self.create_subscription(
@@ -95,14 +145,23 @@ class MoveDistanceClient(Node):
             10
         )
 
+        # Initialize current position and angle
         self.current_x = 0.0
         self.current_y = 0.0
+        self.current_angle = 0.0
+
+        # Initialize current distance between the anchors
+        self.current_dist_1 = 0.0
+        self.current_dist_2 = 0.0
+
+        
+
 
         # Subscribe to /uwb for tracking movement
-        self.subscription = self.create_subscription(
-            Odometry,
-            '/odom',
-            self.odom_callback,
+        self.subscription2 = self.create_subscription(
+            Range,
+            '/uwb',
+            self.uwb_callback,
             10
         )
         timer_period = 0.1  # seconds
@@ -110,21 +169,37 @@ class MoveDistanceClient(Node):
         self.status = 0
         self.flag = True
         
+        # Parameters for the relative localization
+        self.ancor_dist = 2
+
         
 
     def timer_callback(self):
         
         if self.status == 0:
             if self.flag:
+                # self.D_k1 = self.compute_distance_matrix(self.current_dist_1, self.current_dist_2, self.ancor_dist)
                 self.send_goal(1.0, 0.0)
                 self.flag = False
         if self.status == 1:
             if self.flag:
-                self.send_goal(0.0, 1.0)
+                self.send_rotation(90.0)
                 self.flag = False
         if self.status == 2:
+            if self.flag:
+                self.send_goal(0.0, 1.0)
+                self.flag = False
+                # os.system('ros2 run ')
+        if self.status == 3:
             self.get_logger().info('Both goals successfully reached! Starting relative localization')
             ## relative localization
+            if self.flag:
+                self.flag = False
+
+                # Find the relative position between the anchors and the robot using where are you
+                
+                # Launch node to control the robot with the keyboard
+                os.system('ros2 run teleop_twist_keyboard teleop_twist_keyboard')
 
             
         
@@ -132,6 +207,17 @@ class MoveDistanceClient(Node):
         """Callback to capture odometry data (current position)."""
         self.current_x = msg.pose.pose.position.x
         self.current_y = msg.pose.pose.position.y
+        orientation_q = msg.pose.pose.orientation
+        orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+        (_, _, yaw) = tf_transformations.euler_from_quaternion(orientation_list)
+        self.current_angle = yaw
+
+    def uwb_callback(self, msg):
+        """Callback to capture uwb data (current distance between the anchors)."""
+        if msg.header.frame_id == 'uwb1':
+            self.current_dist_1 = msg.range
+        elif msg.header.frame_id == 'uwb2':
+            self.current_dist_2 = msg.range
 
     def send_goal(self, x_goal, y_goal):
         """Send a goal to the action server."""
@@ -163,15 +249,47 @@ class MoveDistanceClient(Node):
         """Handle the result from the action server."""
         result = future.result().result
         self.get_logger().info(f'Final position: x: {result.actual_x}, y: {result.actual_y}')
+
+        # Save 
         
         
         self.status += 1
         self.flag = True
+    
+    def send_rotation(self, angle):
+        """Send a goal to the action server."""
+        self.get_logger().info(f'Sending rotation goal to {angle} degrees')
+        
+        goal_msg = Rotate.Goal()
+        goal_msg.rotation_angle = angle
 
-    def perform_other_code(self):
-        """Placeholder for future work after completing goals."""
-        self.get_logger().info("Performing other tasks after reaching goals...")
-        # Here you can add the code that you want to run after the goals are reached.
+        self._action_client_rotate.wait_for_server()
+        send_goal_future = self._action_client_rotate.send_goal_async(goal_msg)
+        send_goal_future.add_done_callback(self.goal_response_callback_rotation)
+
+    def goal_response_callback_rotation(self, future):
+        """Handle the response from the action server."""
+        goal_handle = future.result()
+        
+        if not goal_handle.accepted:
+            self.get_logger().info('Goal rejected')
+            return
+        
+        self.get_logger().info('Goal accepted')
+
+        # Wait for the result of the goal
+        get_result_future = goal_handle.get_result_async()
+        get_result_future.add_done_callback(self.get_result_callback_rotation)
+    
+    def get_result_callback_rotation(self, future):
+        """Handle the result from the action server."""
+        result = future.result().result
+        self.get_logger().info(f'Final angle: {result.actual_rotation_angle}')
+        
+        self.status += 1
+        self.flag = True
+
+    
 
 def main(args=None):
     rclpy.init(args=args)
--- where_are_you_ros2/where_are_you_ros2/where_are_you_action_server.py
+++ where_are_you_ros2/where_are_you_ros2/where_are_you_action_server.py
@@ -6,19 +6,44 @@ from rclpy.node import Node
 from geometry_msgs.msg import Twist  # For publishing velocity
 from nav_msgs.msg import Odometry    # For subscribing to /odom
 from action_move_interfaces.action import MoveDistance  # Import your custom action
+from action_move_interfaces.action import Rotate
 import math
+import time
+from rclpy.callback_groups import MutuallyExclusiveCallbackGroup
+from rclpy.executors import MultiThreadedExecutor
+import tf_transformations 
+
+
+# def move_robot(goal_x, goal_y, current_x, current_y):
+#     # simulate movement 
+#     current_x += goal_x/100
+#     current_y += goal_y/100
+
+#     return current_x, current_y
 
 class MoveDistanceServer(Node):
 
     def __init__(self):
         super().__init__('move_distance_server')
 
+        self.action_ = MutuallyExclusiveCallbackGroup()
+        self.topic_ = MutuallyExclusiveCallbackGroup()
+
         # Action server to handle the move_distance action
         self._action_server = ActionServer(
             self,
             MoveDistance,
             'move_distance',
-            self.execute_callback
+            self.execute_callback,
+            callback_group=self.action_
+        )
+
+        self._action_server_rotate = ActionServer(
+            self,
+            Rotate,
+            'rotate',
+            self.execute_callback_rotate,
+            callback_group=self.action_
         )
 
         # Publisher for /cmd_vel (velocity commands)
@@ -29,17 +54,29 @@ class MoveDistanceServer(Node):
             Odometry,
             '/odom',
             self.odom_callback,
-            10
+            10,
+            callback_group=self.topic_
         )
 
         self.current_x = 0.0
         self.current_y = 0.0
+        self.current_angle = 0.0
+        self.start_x = 0.0
+        self.start_y = 0.0
         self.get_logger().info('Move Distance Action Server is ready.')
 
     def odom_callback(self, msg):
         """Update current position from /odom topic."""
-        self.current_x = msg.pose.pose.position.x
-        self.current_y = msg.pose.pose.position.y
+        if msg.child_frame_id == 'base_link':
+            self.current_x = msg.pose.pose.position.x
+            self.current_y = msg.pose.pose.position.y
+            # Convert quaternion to Euler angles
+            orientation_q = msg.pose.pose.orientation
+            orientation_list = [orientation_q.x, orientation_q.y, orientation_q.z, orientation_q.w]
+            (_, _, yaw) = tf_transformations.euler_from_quaternion(orientation_list)
+            self.current_angle = yaw
+            # print(f"Current angle: {self.current_angle}")
+            # print(f"Current position: x:{self.current_x} | y:{self.current_y}")
 
     def execute_callback(self, goal_handle):
         """Handle goal execution and publish velocity commands."""
@@ -51,13 +88,16 @@ class MoveDistanceServer(Node):
 
         goal_x = goal_handle.request.x
         goal_y = goal_handle.request.y
+        
+        self.start_x = self.current_x
+        self.start_y = self.current_y
 
         # Calculate the distance to the goal
         total_distance = self.get_distance_to_goal(goal_x, goal_y)
         linear_speed = 0.5  # Fixed linear speed (m/s)
         tolerance = 0.05  # Stopping tolerance
 
-        while total_distance > tolerance:
+        while total_distance < goal_x**2 + goal_y**2:
             if goal_handle.is_cancel_requested:
                 goal_handle.canceled()
                 self.get_logger().info('Goal canceled')
@@ -75,28 +115,94 @@ class MoveDistanceServer(Node):
             # Move forward if aligned with the goal direction
             twist_msg.linear.x = linear_speed
 
+            # Simulate movement 
+            # self.current_x, self.current_y = move_robot(goal_x, goal_y, self.current_x, self.current_y)
+
             # Publish the velocity command
             self.publisher_.publish(twist_msg)
 
-            # Update feedback and publish
+            # # Update feedback and publish
             feedback_msg.current_x = self.current_x
             feedback_msg.current_y = self.current_y
             goal_handle.publish_feedback(feedback_msg)
+            # self.get_logger().info(f"Feedback: current position: x:{self.current_x} | y:{self.current_y}")
+            # Print total distance
+            self.get_logger().info(f"Total distance to goal: {total_distance}")
+            time.sleep(0.1)   
 
         # Stop the robot after reaching the goal
         self.stop_robot()
 
         # Set result when the goal is reached
-        result_msg.x_actual = self.current_x
-        result_msg.y_actual = self.current_y
+        result_msg.actual_x = self.current_x
+        result_msg.actual_y = self.current_y
         goal_handle.succeed()
 
         self.get_logger().info(f'Goal succeeded. Final position: x: {self.current_x}, y: {self.current_y}')
         return result_msg
+    
+    def execute_callback_rotate(self, goal_handle):
+        # Function that reads the goal and executes the action
+        self.get_logger().info(f'Executing goal to angle: {goal_handle.request.rotation_angle}')
+
+        # Feedback and result initialization
+        feedback_msg = Rotate.Feedback()
+        result_msg = Rotate.Result()
+        goal_angle = goal_handle.request.rotation_angle * math.pi / 180  # Convert to radians
+        angular_speed = 0.5  # Fixed angular speed (rad/s)
+        tolerance = 0.01  # Stopping tolerance
+
+        # Current rotated angle
+        rotated_angle = 0.0
+
+        # Starting orientation from quaternion in odom to Euler angle
+        starting_angle = self.current_angle
+
+        print(f"Goal angle: {goal_angle}")
+        print(f"Rotated angle: {rotated_angle}")
+        print(f"Starting angle: {starting_angle}")
+
+        while abs(rotated_angle) < abs(goal_angle):
+            if goal_handle.is_cancel_requested:
+                goal_handle.canceled()
+                self.get_logger().info('Goal canceled')
+                return Rotate.Result()
+
+            # Create a Twist message to command the robot
+            twist_msg = Twist()
+            # Wrap the angle to [-pi, pi]
+            remaining_angle = goal_angle - rotated_angle
+            remaining_angle = (remaining_angle + math.pi) % (2 * math.pi) - math.pi
+
+            twist_msg.angular.z = angular_speed if remaining_angle > 0 else -angular_speed
+
+            # Publish the velocity command
+            self.publisher_.publish(twist_msg)
+
+            time.sleep(0.1)
+
+            # Calculate the rotated angle
+            rotated_angle = starting_angle - self.current_angle
+            rotated_angle = (rotated_angle + math.pi) % (2 * math.pi) - math.pi
+            
+            # Update feedback and publish
+            feedback_msg.current_rotation_angle = rotated_angle
+            goal_handle.publish_feedback(feedback_msg)
+            self.get_logger().info(f"Feedback: current angle: {feedback_msg.current_rotation_angle}")
+
+        # Stop the robot after reaching the goal
+        self.stop_robot()
+
+        # Set result when the goal is reached
+        result_msg.actual_rotation_angle = goal_angle
+        goal_handle.succeed()
+
+        self.get_logger().info(f'Goal succeeded. Final angle: {goal_angle}')
+        return result_msg
 
     def get_distance_to_goal(self, goal_x, goal_y):
         """Calculate Euclidean distance to the goal."""
-        return math.sqrt((goal_x - self.current_x)**2 + (goal_y - self.current_y)**2)
+        return math.sqrt((self.current_x - self.start_x)**2 + (self.current_y - self.start_y)**2)
 
     def stop_robot(self):
         """Send a zero velocity command to stop the robot."""
@@ -109,7 +215,14 @@ def main(args=None):
 
     action_server = MoveDistanceServer()
 
-    rclpy.spin(action_server)
+    executor = MultiThreadedExecutor()
+    executor.add_node(action_server)
+
+    try:
+        executor.spin()
+
+    except KeyboardInterrupt:
+        action_server.get_logger().error("Caught keyboard interrupt")
 
     action_server.destroy()
     rclpy.shutdown()
